3cf9476a063ae8551a8f34a0486649a92c8b174d
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-04-19 12:27:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Market76::Application.config.secret_key_base = '4800d97edebea6aa946b651d3c19cff0cc4ecfbf193b8df0570247ae6077be9676549dbe24aa3c103d92ee1351e160d62f4e7ff320cba33c9d1a8af6483a16a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Market76::Application.config.session_store :cookie_store, key: '_market76_session'
